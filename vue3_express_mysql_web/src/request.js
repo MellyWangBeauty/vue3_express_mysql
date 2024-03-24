27c137a395653ebd53e325663daf85f3c04fe121
MellyWang <2212491506@qq.com>
2024-03-24 22:02:27 +0800

    环境配置

--- vue3_express_mysql_web/src/request.js
+++ vue3_express_mysql_web/src/request.js
@@ -3,13 +3,8 @@ import axios from "axios";
 
 // 创建axios实例
 const service = axios.create({
-  // axios中请求配置有baseURL选项，表示请求URL公共部分
-  // baseURL: process.env.VUE_APP_BASE_API,
-  // baseURL: "/api",
-  // baseURL: "http://localhost:5000",
-  baseURL: "http://8.219.89.226:5000",
-  // 超时
-  timeout: 20000,
+  baseURL: import.meta.env.VITE_APP_BASE_URL,
+  timeout: 20000, // 超时
 });
 
 export default service;
--- vue3_express_mysql_web/vite.config.js
+++ vue3_express_mysql_web/vite.config.js
@@ -1,23 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000, // 设置开发服务器的端口号为3000
-    // proxy: {
-    //   "/api": {
-    //     target: "http://localhost:5000", //后台地址
-    //     changeOrigin: true, //开启跨域
-    //     rewrite: (path) => path.replace(/^\/api/, ""),
-    //   },
-    // },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd()); // 获取 .env里面定义是参数
+  return {
+    plugins: [vue()],
+    server: {
+      host: "0.0.0.0",
+      open: true,
+      port: 3000, //端口号
+      proxy: {
+        // 本地开发环境通过代理实现跨域
+        // 正则表达式写法
+        [env.VITE_APP_BASE_API]: {
+          target: env.VITE_APP_BASE_URL, // 后端服务实际地址
+          changeOrigin: true, //开启代理
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
     },
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+  };
 });
